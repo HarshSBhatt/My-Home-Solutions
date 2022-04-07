166aeddf3d2a41f1e8368a99b6427d12be147fd8
hbhatt <hr559239@dal.ca>
2022-04-07 14:04:08 -0300

    error page modified

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,37 @@
 // Author: Harsh Bhatt (B00877053)
 
+import { Typography, Box, Button } from "@mui/material";
+import { ROUTES } from "common/constants";
+import { useNavigate } from "react-router-dom";
+
 function Error() {
-  return <div>This is error page</div>;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(ROUTES.HOME);
+  };
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      sx={{ height: "calc(100vh - 64px)" }}
+    >
+      <Typography
+        sx={{ fontSize: 24 }}
+        variant="overline"
+        display="block"
+        gutterBottom
+        align="center"
+      >
+        This page doesn't seem to exist or you do not have privilege to access
+        this page
+      </Typography>
+      <Button variant="contained" onClick={handleClick}>
+        Go to Home
+      </Button>
+    </Box>
+  );
 }
 
 export default Error;
